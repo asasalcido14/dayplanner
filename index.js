b1f2b92bbfbfde463396152b67f4d2439f194bf2
Asa Salcido <asasalcido14@email.arizona.edu>
2020-10-26 19:48:56 -0700

    added document ready, button click, variables for local storage

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 // use jquery to start function
 $(document).ready(function() {
+// identify button to save sBtn
+$(".sBtn").on("click", function() {
+// use prompt and slot variable to provide parameters for local storage
+const prompt = $(this).siblings(".tasks").val();
+const slot = $(this).parent().attr("timestamp");
 
-    
+// tasks the user wants to save should be stored in local storage 
+localStorage.setItem(slot, prompt);
+
+});
+// how to include moment w/ the app? 
+
+
 }
